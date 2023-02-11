83e568da2338c2aa3bb3b66bc8dcc008fe4bb8f8
eyurkin <ave2008@gmail.com>
2023-02-11 17:57:12 +0300

    hange

---
+++ main-svc/src/main/java/ru/practicum/test/WhitePage.java
@@ -0,0 +1,18 @@
+package ru.practicum.test;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/")
+@RequiredArgsConstructor
+@Slf4j
+@Validated
+public class WhitePage {
+    @GetMapping
+    public String welcome() {
+        return "login";
+    }
+}
