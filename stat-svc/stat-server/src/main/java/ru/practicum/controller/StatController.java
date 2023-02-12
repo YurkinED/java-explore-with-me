b1a934f696a6c89191e34faa06b1cc017f5fba8a
eyurkin <ave2008@gmail.com>
2023-02-12 14:18:53 +0300

    Update StatController.java

--- stat-svc/stat-server/src/main/java/ru/practicum/controller/StatController.java
+++ stat-svc/stat-server/src/main/java/ru/practicum/controller/StatController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.EventStatDto;
 import ru.practicum.ViewStats;
