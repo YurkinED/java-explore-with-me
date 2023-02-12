02164116a0afa203569f2d55acb91f3153d6c256
eyurkin <ave2008@gmail.com>
2023-02-12 21:51:10 +0300

    Update MainService.java

--- main-svc/src/main/java/ru/practicum/MainService.java
+++ main-svc/src/main/java/ru/practicum/MainService.java
@@ -3,8 +3,6 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCaching
