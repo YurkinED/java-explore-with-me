bd9489568df31c1ab1f4683021e0c6a493b3e30b
eyurkin <ave2008@gmail.com>
2023-02-16 21:33:14 +0300

    Update Client.java

--- stat-svc/stat-client/src/main/java/ru/practicum/Client.java
+++ stat-svc/stat-client/src/main/java/ru/practicum/Client.java
@@ -1,12 +1,10 @@
 package ru.practicum;
 
-import io.micrometer.core.lang.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
 
 @RequiredArgsConstructor
 public class Client {
